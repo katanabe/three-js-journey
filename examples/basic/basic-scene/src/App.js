04d62a2f335113a49812ef575e0e8a7cc4b0ff1e
katanabe <nabeon+github@gmail.com>
2022-04-18 14:01:35 +0900

    Fix

--- examples/basic/basic-scene/src/App.js
+++ examples/basic/basic-scene/src/App.js
@@ -3,42 +3,40 @@ import './App.css';
 import {
   Scene,
   BoxGeometry,
-  MeshBasicMaterial,
   Mesh,
   PerspectiveCamera,
   WebGLRenderer,
+  MeshNormalMaterial,
 } from 'three';
 
-const renderer = (ref) => {
-  const sizes = {
-    width: 800,
-    height: 600,
-  };
+const renderer = (targetDom) => {
+  const camera = new PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 1;
 
   const scene = new Scene();
 
-  const cubeGeometry = new BoxGeometry(1, 1, 1);
-  const cubeMaterial = new MeshBasicMaterial({
-    color: '#ff0000',
-  });
-  const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
-  scene.add(cubeMesh);
+  const geometry = new BoxGeometry(0.2, 0.2, 0.2);
+  const material = new MeshNormalMaterial();
 
-  const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
-  camera.position.z = 3;
-  scene.add(camera);
+  const mesh = new Mesh(geometry, material);
+  scene.add(mesh);
 
-  const renderer = new WebGLRenderer({
-    canvas: ref.current,
-  });
-  renderer.setSize(sizes.width, sizes.height);
+  const animation = (time) => {
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
 
-  function animate() {
-    requestAnimationFrame(animate);
     renderer.render(scene, camera);
-  }
+  };
 
-  animate();
+  const renderer = new WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  targetDom.appendChild(renderer.domElement);
 };
 
 function App() {
@@ -49,18 +47,10 @@ function App() {
       return;
     }
 
-    const canvas = ref.current;
-    const context = canvas.getContext('2d');
-    console.log(context);
-
-    context.strokeStyle = '#000000'; // 矩形色
-    context.lineWidth = 2; // 矩形線幅
-    context.strokeRect(100, 100, 100, 100); // 矩形描画
-
     renderer(ref.current);
   }, [ref]);
 
-  return <canvas ref={ref} style={{ width: '100%', height: '100%' }} />;
+  return <div ref={ref} />;
 }
 
 export default App;
