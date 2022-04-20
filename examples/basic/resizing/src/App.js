8deede49aa2024f63794dc1350539af4607a50c8
katanabe <nabeon+github@gmail.com>
2022-04-20 22:26:04 +0900

    Add fullscreen and resizing lesson.

---
+++ examples/basic/resizing/src/App.js
@@ -0,0 +1,107 @@
+import { useEffect, useRef } from 'react';
+import {
+  Scene,
+  BoxGeometry,
+  Mesh,
+  PerspectiveCamera,
+  WebGLRenderer,
+  MeshBasicMaterial,
+  Clock,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const renderer = (targetDom) => {
+  let cursor = {
+    x: 0,
+    y: 0,
+  };
+
+  // Objects
+  const mesh = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshBasicMaterial({ color: 0xff0000 })
+  );
+
+  // Camera
+  const camera = new PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
+  );
+  camera.position.z = 3;
+
+  // Clock
+  // const clock = new Clock();
+
+  // Scene
+  const scene = new Scene();
+  scene.add(mesh);
+  scene.add(camera);
+
+  const controls = new OrbitControls(camera, targetDom);
+  // controls.enabled = false;
+  controls.enableDamping = true;
+
+  const animation = () => {
+    // const elapsedTime = clock.getElapsedTime();
+
+    // Update camera
+    // camera.position.x = Math.cos(cursor.x * Math.PI * 2) * 3;
+    // camera.position.y = cursor.y * 5;
+    // camera.position.z = Math.sin(cursor.x * Math.PI * 2) * 3;
+    // // camera.position.x = Math.cos(elapsedTime);
+    // camera.lookAt(mesh.position);
+
+    controls.update();
+
+    renderer.render(scene, camera);
+  };
+
+  const renderer = new WebGLRenderer({ antialias: true });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  targetDom.appendChild(renderer.domElement);
+
+  window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / window.innerWidth - 0.5;
+    cursor.x = event.clientY / window.innerHeight - 0.5;
+  });
+
+  window.addEventListener('resize', (event) => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    // update camer
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(w, h);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  window.addEventListener('dblclick', () => {
+    if (window.document.fullscreenEnabled) {
+      renderer.domElement.requestFullscreen();
+    } else {
+      renderer.domElement.exitFullscreen();
+    }
+  });
+};
+
+function App() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    renderer(ref.current);
+  }, [ref]);
+
+  return <div ref={ref} />;
+}
+
+export default App;
