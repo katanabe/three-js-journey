108ddc469e2ce570359489a081d30e07402c8702
katanabe <nabeon+github@gmail.com>
2022-05-16 09:38:17 +0900

    Modify template.

--- examples/__template__/src/App.js
+++ examples/__template__/src/App.js
@@ -1,24 +1,92 @@
 import { useEffect, useRef } from 'react';
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { SphereGeometry } from 'three';
+import { PlaneGeometry } from 'three';
+import { Mesh } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, Clock } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as lil from 'lil-gui';
 
 const renderer = (targetDom) => {
-  // Camera
+  // Debug
+  const gui = new lil.GUI();
+
+  // Scene
+  const scene = new Scene();
+
+  /**
+   * Materials
+   */
+
+  /**
+   * Objects
+   */
+  const sphere = new Mesh(new SphereGeometry(0.5, 32, 32), material);
+
+  const plane = new Mesh(new PlaneGeometry(5, 5), material);
+  plane.rotation.x = -Math.PI * 0.5;
+  plane.position.y = -0.5;
+
+  scene.add(sphere, plane);
+
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  /**
+   * Camera
+   */
   const camera = new PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight
+    sizes.width / sizes.height,
+    0.1,
+    100
   );
-  camera.position.z = 3;
-
-  const scene = new Scene();
+  camera.position.x = 1;
+  camera.position.y = 1;
+  camera.position.z = 2;
   scene.add(camera);
 
-  const animation = (_time) => {
+  // Controls
+  const controls = new OrbitControls(camera, targetDom);
+  controls.enableDamping = true;
+
+  /**
+   * Animate
+   */
+  const clock = new Clock();
+
+  const animation = () => {
+    // const elapsedTime = clock.getElapsedTime();
+
+    // Update controls
+    controls.update();
+
+    // Render
     renderer.render(scene, camera);
   };
 
   const renderer = new WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setAnimationLoop(animation);
+
   targetDom.appendChild(renderer.domElement);
 };
 
